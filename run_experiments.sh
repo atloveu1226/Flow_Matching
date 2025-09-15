0da6de1ff2975688f24b900fb7c8b17d0e4b9c15
alan <yifan3797@gmail.com>
2025-09-15 23:30:57 +0800

    new train

--- run_experiments.sh
+++ run_experiments.sh
@@ -10,33 +10,40 @@ CONFIGS=(
     # "FM/custom_configs/other_config.py"
 )
 
-NUM_STEPS_LIST=(20000)
-BATCH_SIZE_LIST=(256)
+#NUM_STEPS_LIST=(20000)
+#BATCH_SIZE_LIST=(256)
+
+KEY_LIST=(1 2 3 4 5 43 6 7 8 9)
+NUM_ITER=5
 
 for CONFIG in "${CONFIGS[@]}"; do
-    for NUM_STEPS in "${NUM_STEPS_LIST[@]}"; do
-        for BATCH_SIZE in "${BATCH_SIZE_LIST[@]}"; do
-            echo "========================================"
-            echo "Running experiment:"
-            echo "  Config:      $CONFIG"
-            echo "  Num steps:   $NUM_STEPS"
-            echo "  Batch size:  $BATCH_SIZE"
-            echo "----------------------------------------"
-            python /Users/alan/PyCharmMiscProject/Flow_Matching/FM/train.py \
-                --config="$CONFIG" \
-                --mode=train \
-                --device=cpu \
-                --config.train.num_iter="$NUM_STEPS" \
-                --config.train.batch_size="$BATCH_SIZE"\
-                --num_steps=10
-            STATUS=$?
-            if [ $STATUS -eq 0 ]; then
-                echo "Finished successfully."
-            else
-                echo "Experiment failed with exit code $STATUS."
-            fi
-            echo "========================================"
-            echo
-        done
+    for key in "${KEY_LIST[@]}"; do
+        for i in $(seq 1 $NUM_ITER); do
+        echo "========================================"
+        echo "Running experiment:"
+        echo "Iteration $i of $NUM_ITER"
+        echo "  Config:      $CONFIG"
+        echo "  Num steps:   100000"
+        echo "  Batch size:  256"
+        echo "  Key:         $key"
+        echo "----------------------------------------"
+        python /Users/alan/PyCharmMiscProject/Flow_Matching/FM/train.py \
+            --config="$CONFIG" \
+            --mode=train \
+            --device=cpu \
+            --config.train.num_iter=100000 \
+            --config.train.batch_size=256\
+            --config.train.key="$key" \
+            --num_steps=10
+        STATUS=$?
+        if [ $STATUS -eq 0 ]; then
+            echo "Finished successfully."
+        else
+            echo "Experiment failed with exit code $STATUS."
+        fi
+        echo "========================================"
+        echo
     done
+  done
 done
+
