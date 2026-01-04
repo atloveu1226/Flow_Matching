d98bc628a41c4c511b60730463ff4e4773d1542a
isometricat <isometricat@icloud.com>
2026-01-04 19:00:33 +0000

    add script

--- run_graph.sh
+++ run_graph.sh
@@ -1,26 +1,17 @@
-export KMP_DUPLICATE_LIB_OK=TRUE
+NUM_ITER=10
+SAMPLE_INTERVAL=2
+EVAL_INTERVAL=2
+BS=1
+EVAL_BS=1
 
-CONFIGS=(
-    "FM/custom_configs/esd_graph.py"
-    #"/Users/alan/PyCharmMiscProject/Flow_Matching/FM/custom_configs_graph/esd_graph.py"
-    # Add more config files here, e.g.:
-    # "FM/custom_configs/other_config.py"
-)
+DEVICE="cuda"
 
-KEY_LIST=(1)
-NUM_TRAIN_STEPS=100
-BATCH_SIZE=64
-
-for CONFIG in "${CONFIGS[@]}"; do
-    for key in "${KEY_LIST[@]}"; do
-        # ...
-        python FM/train_graph.py \
-            --config="$CONFIG" \
-            --config.train.num_iter=$NUM_TRAIN_STEPS \
-            --config.train.batch_size=$BATCH_SIZE \
-            --config.train.key="$key" \
-            --config.train.sample_interval=50 \
-            --config.train.eval_interval=50 \
-            --num_steps=10
-  done
-done
+python FM/train_img.py --config=FM/custom_configs/image.py \
+  --device=$DEVICE \
+  --config.train.num_iter=$NUM_ITER \
+  --config.train.lr=5e-4 \
+  --config.train.sample_interval=$SAMPLE_INTERVAL \
+  --config.train.eval_interval=$EVAL_INTERVAL \
+  --config.train.eval_bs=$EVAL_BS \
+  --config.train.batch_size=$BS \
+  --config.evaluation.eval_batch_size=$EVAL_BS
